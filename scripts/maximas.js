8ed99f979214d6f1e0e4a728f4cc983a90cc3e0a
alexwca <alexwcaffonso@gmail.com>
2024-06-30 16:03:00 -0300

    adjustment of the current maximum count

--- scripts/maximas.js
+++ scripts/maximas.js
@@ -28,52 +28,60 @@ function generateMaximasDeTimes() {
         const [score1, score2] = match.score.split('-').map(Number);
 
         if (!teamStats[team1]) {
-            teamStats[team1] = { currentStreak: 0, maxNoWinsStreak: 0, sinceLastWin: 0 };
+            teamStats[team1] = { currentWithoutWin: 0, maxNoWinsStreak: 0 };
         }
         if (!teamStats[team2]) {
-            teamStats[team2] = { currentStreak: 0, maxNoWinsStreak: 0, sinceLastWin: 0 };
+            teamStats[team2] = { currentWithoutWin: 0, maxNoWinsStreak: 0 };
         }
 
         if (score1 > score2) {
             // Team 1 wins, reset its current streak
-            teamStats[team1].sinceLastWin = 0;
-            teamStats[team2].sinceLastWin++;
-            if (teamStats[team2].sinceLastWin > teamStats[team2].maxNoWinsStreak) {
-                teamStats[team2].maxNoWinsStreak = teamStats[team2].sinceLastWin;
+            teamStats[team1].currentWithoutWin = 0;
+
+            // Update Team 2
+            teamStats[team2].currentWithoutWin++;
+            if (teamStats[team2].currentWithoutWin > teamStats[team2].maxNoWinsStreak) {
+                teamStats[team2].maxNoWinsStreak = teamStats[team2].currentWithoutWin;
             }
         } else if (score2 > score1) {
             // Team 2 wins, reset its current streak
-            teamStats[team2].sinceLastWin = 0;
-            teamStats[team1].sinceLastWin++;
-            if (teamStats[team1].sinceLastWin > teamStats[team1].maxNoWinsStreak) {
-                teamStats[team1].maxNoWinsStreak = teamStats[team1].sinceLastWin;
+            teamStats[team2].currentWithoutWin = 0;
+
+            // Update Team 1
+            teamStats[team1].currentWithoutWin++;
+            if (teamStats[team1].currentWithoutWin > teamStats[team1].maxNoWinsStreak) {
+                teamStats[team1].maxNoWinsStreak = teamStats[team1].currentWithoutWin;
             }
         } else {
             // Draw, update both teams' streaks
-            teamStats[team1].sinceLastWin++;
-            teamStats[team2].sinceLastWin++;
-            if (teamStats[team1].sinceLastWin > teamStats[team1].maxNoWinsStreak) {
-                teamStats[team1].maxNoWinsStreak = teamStats[team1].sinceLastWin;
+            teamStats[team1].currentWithoutWin++;
+            teamStats[team2].currentWithoutWin++;
+            if (teamStats[team1].currentWithoutWin > teamStats[team1].maxNoWinsStreak) {
+                teamStats[team1].maxNoWinsStreak = teamStats[team1].currentWithoutWin;
             }
-            if (teamStats[team2].sinceLastWin > teamStats[team2].maxNoWinsStreak) {
-                teamStats[team2].maxNoWinsStreak = teamStats[team2].sinceLastWin;
+            if (teamStats[team2].currentWithoutWin > teamStats[team2].maxNoWinsStreak) {
+                teamStats[team2].maxNoWinsStreak = teamStats[team2].currentWithoutWin;
             }
         }
 
-        // Set the current streak for the final result
-        teamStats[team1].currentStreak = teamStats[team1].sinceLastWin;
-        teamStats[team2].currentStreak = teamStats[team2].sinceLastWin;
+        if(teamStats[team1] == "Coreia do Sul") {
+
+            console.log(a)
+        }
     });
+
+
+    
 
     // Transformar os dados do objeto em uma array para ordenação
     const teamStatsArray = Object.keys(teamStats).map(team => ({
         team,
-        currentStreak: teamStats[team].currentStreak,
+        currentWithoutWin: teamStats[team].currentWithoutWin,
         maxNoWinsStreak: teamStats[team].maxNoWinsStreak
     }));
 
     // Ordenar pela coluna "Máxima Atual" em ordem decrescente
-    teamStatsArray.sort((a, b) => b.currentStreak - a.currentStreak);
+    teamStatsArray.sort((a, b) => b.maxNoWinsStreak - a.maxNoWinsStreak);
 
     const maximasTable = document.getElementById('maximasTable');
     const tbody = maximasTable.querySelector('tbody');
@@ -83,7 +91,7 @@ function generateMaximasDeTimes() {
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>${teamStat.team}</td>
-            <td>${teamStat.currentStreak}</td>
+            <td>${teamStat.currentWithoutWin}</td>
             <td>${teamStat.maxNoWinsStreak}</td>
         `;
         tbody.appendChild(row);
