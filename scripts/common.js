4ff6a05b459b23d0d2b479819e42e1615621a8ff
alexwca <alexwcaffonso@gmail.com>
2024-07-01 22:16:18 -0300

    adjustments

--- scripts/common.js
+++ scripts/common.js
@@ -4,14 +4,19 @@ function clearData() {
     if (document.getElementById('negativeInput')) {
         document.getElementById('negativeInput').value = "";
     }
-    document.getElementById('resultsTable').style.display = 'none';
-    if (document.getElementById('matchTable')) {
-        document.getElementById('matchTable').style.display = 'none';
+    if (document.getElementById('resultsTable')) {
+        document.getElementById('resultsTable').style.display = 'none';
     }
     if (document.getElementById('missingTeamsTable')) {
         document.getElementById('missingTeamsTable').style.display = 'none';
     }
     if (document.getElementById('maximasTable')) {
         document.getElementById('maximasTable').style.display = 'none';
+    }
+    if (document.getElementById('statsTable')) {
+        document.getElementById('statsTable').style.display = 'none';
+    }
+    if (document.getElementById('matchTable')) {
+        document.getElementById('matchTable').style.display = 'none';
     }
 }
